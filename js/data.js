74d07913d9ab3bf064337d15621dca7fb3b0b991
masarusz <281866484+masarusz@users.noreply.github.com>
2026-09-12 21:51:55 +0900

    Publish 2026-09-12T12:51:55Z

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
-import { VERSION } from './version.js?v=0.5.1';
-import { isIsoDate } from './ages.js?v=0.5.1';
+import { VERSION } from './version.js?v=0.5.2';
+import { isIsoDate } from './ages.js?v=0.5.2';
 
 const cache = new Map();
 
